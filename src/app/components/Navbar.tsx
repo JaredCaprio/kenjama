0034b4708b51facc1bb4b622ac9de1cad6f5bbbc
JaredCaprio <88853874+JaredCaprio@users.noreply.github.com>
2024-05-13 17:33:45 -0400

    mobile navbar added

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,15 +1,22 @@
 'use client';
-import React from 'react';
+import React, { useState } from 'react';
 import Button from '../components/Button';
 import ThemeToggle from './ThemeToggle';
 import { useTheme } from 'next-themes';
 import ThemedImage from './ThemedImage';
+import { IoMdClose } from 'react-icons/io';
+import { RxHamburgerMenu } from 'react-icons/rx';
 
 type Props = {};
 
 export default function Navbar({}: Props) {
+	const [menuOpen, setMenuOpen] = useState(false);
 	const { resolvedTheme } = useTheme();
 	let src = resolvedTheme === 'dark' ? '/dark-mode-logo.png' : '/logo.png';
+
+	const handleSetMenu = () => {
+		setMenuOpen(!menuOpen);
+	};
 
 	switch (resolvedTheme) {
 		case 'light':
@@ -22,19 +29,53 @@ export default function Navbar({}: Props) {
 
 	return (
 		<div>
-			<nav className='flex justify-between m-5'>
+			<nav className='flex justify-between items-center m-5 '>
 				<ThemedImage lightImage='/logo.png' darkImage='/dark-mode-logo.png' />
-				<ul className='flex justify-between items-center'>
-					<li className='p-5 '>
-						<Button btnContent='Log in' hover={false}></Button>
+				<RxHamburgerMenu
+					onClick={handleSetMenu}
+					className={`${
+						!menuOpen ? 'sm:hidden z-20' : 'hidden'
+					} cursor-pointer min-w-10`}
+					size={30}
+				/>
+				<IoMdClose
+					onClick={handleSetMenu}
+					className={`${
+						menuOpen ? 'sm:hidden z-20' : 'hidden'
+					} cursor-pointer min-w-10`}
+					size={30}
+				/>
+				<ul className='hidden justify-between items-center sm:flex'>
+					<li className='p-5'>
+						<Button willHover={false}>Log in</Button>
 					</li>
 					<li className='p-5'>
 						<Button
-							hover={true}
-							btnContent='Sign Up'
+							willHover={true}
 							textColor='text-white'
 							bgColor='bg-primary/100'
-						/>
+						>
+							Sign Up
+						</Button>
+					</li>
+					<ThemeToggle />
+				</ul>
+			</nav>
+			<nav
+				className={
+					menuOpen
+						? 'fixed top-0 right-0 w-[60%] sm:hidden h-screen bg-background p-10 ease-in duration-200 z-10 opacity-85'
+						: 'fixed left-[-100%]'
+				}
+			>
+				<ul className='flex flex-col justify-between items-center space-y-10'>
+					<li>
+						<Button willHover={false}>Log in</Button>
+					</li>
+					<li>
+						<Button willHover={false} textColor='text-content'>
+							Sign Up
+						</Button>
 					</li>
 					<ThemeToggle />
 				</ul>
