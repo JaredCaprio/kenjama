7d77185dee391147c020bbf8fbb084e9376b6c75
JaredCaprio <88853874+JaredCaprio@users.noreply.github.com>
2024-09-03 11:40:12 -0400

    fixed scoping issue for firebase-next-auth-edge login api endpoint / moved sign-in and sign-out methods into authContext provider

--- src/contexts/authContext.tsx
+++ src/contexts/authContext.tsx
@@ -6,25 +6,21 @@ import {
     useEffect,
     ReactNode,
 } from 'react';
-import { onAuthStateChanged } from 'firebase/auth';
-import { firebaseAuth } from '../config/firebase-config';
-import { useRouter } from 'next/navigation';
-
-export type UserState = {
-    displayName: string | null | undefined;
-    email: string | null | undefined;
-    photoURL: string | null | undefined;
-    uid: string;
-};
-
-interface AuthContextType {
-    user: UserState | null;
-    loading: boolean;
-}
+import {
+    GoogleAuthProvider,
+    onAuthStateChanged,
+    signInWithPopup,
+} from 'firebase/auth';
+import { db, firebaseAuth } from '../config/firebase-config';
+import { doc, getDoc, setDoc } from 'firebase/firestore';
+import { AppRouterInstance } from 'next/dist/shared/lib/app-router-context.shared-runtime';
+import { AuthContextType, UserState } from '@/types/context/Auth';
 
 const AuthContext = createContext<AuthContextType>({
     user: null,
     loading: true,
+    signInWithGoogle: () => {},
+    signOutWithGoogle: () => {},
 });
 
 type props = {
@@ -48,8 +44,48 @@ export const AuthContextProvider = ({ children }: props) => {
         return () => unsubscribe();
     }, []);
 
+    async function signInWithGoogle(router: AppRouterInstance) {
+        const provider = new GoogleAuthProvider();
+        try {
+            const result = await signInWithPopup(firebaseAuth, provider);
+            const user = result.user;
+
+            const userDocRef = doc(db, 'users', result.user.uid);
+
+            const userSnap = await getDoc(userDocRef);
+            const idToken = await user.getIdToken();
+
+            if (!userSnap.exists()) {
+                await setDoc(userDocRef, {
+                    displayName: user.displayName,
+                    email: user.email,
+                    uid: user.uid,
+                    photoURL: user.photoURL,
+                });
+            }
+            await fetch(`/api/login`, {
+                headers: {
+                    Authorization: `Bearer ${idToken}`,
+                },
+            });
+            router.push('/dashboard');
+        } catch (error) {
+            console.error('Error signing in with Google', error);
+        }
+    }
+
+    async function signOutWithGoogle() {
+        try {
+            await firebaseAuth.signOut();
+        } catch (error) {
+            console.error('Error signing out with Google', error);
+        }
+    }
+
     return (
-        <AuthContext.Provider value={{ user, loading }}>
+        <AuthContext.Provider
+            value={{ user, loading, signInWithGoogle, signOutWithGoogle }}
+        >
             {children}
         </AuthContext.Provider>
     );
