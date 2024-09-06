badf83d7e9f1b91cf837bce1faaeadadf1a7c1bb
JaredCaprio <88853874+JaredCaprio@users.noreply.github.com>
2024-09-06 19:02:57 -0400

    added edit and delete button components

--- src/components/HostBadge.tsx
+++ src/components/HostBadge.tsx
@@ -1,23 +1,29 @@
 import Link from 'next/link';
 import AttendanceButton from './Buttons/AttendanceButton';
 import Image from 'next/image';
+import EditButton from './Buttons/EditButton';
+import { Jam } from '@/types/Jam';
+import DeleteButton from './Buttons/DeleteButton';
 
 type HostBadgeProps = {
     photoURL: string;
     displayName: string;
     userId: string | undefined;
-    jamId: string;
+    jamData: Jam;
 };
 
 const HostBadge = ({
     photoURL,
     displayName,
     userId,
-    jamId,
+    jamData,
 }: HostBadgeProps) => {
     return (
-        <div className="mb-10 flex  items-center justify-between  ">
-            <Link className="flex gap-5" href={`/profile/${userId}`}>
+        <div className="mb-10 flex items-center justify-between  ">
+            <Link
+                className="flex gap-5"
+                href={`/profile/${jamData?.hostUser?.uid}`}
+            >
                 <Image
                     src={photoURL}
                     alt="alternative text"
@@ -30,7 +36,15 @@ const HostBadge = ({
                     <p className="font-semibold">{displayName}</p>
                 </div>
             </Link>
-            <AttendanceButton eventId={jamId} userId={userId} />
+            <div>
+                <AttendanceButton eventId={jamData?.id} userId={userId} />
+                {userId === jamData?.hostUser?.uid && (
+                    <>
+                        <EditButton jamData={jamData} />
+                        <DeleteButton />
+                    </>
+                )}
+            </div>
         </div>
     );
 };
